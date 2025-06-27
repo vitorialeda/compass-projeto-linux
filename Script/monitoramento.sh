8f33201f066b55c4a7dc4e344fef18ac3b6c71d6
Vitória <vitoria_lnogueira@hotmail.com>
2025-06-27 18:14:18 -0300

    init

---
+++ Script/monitoramento.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+get_response(){
+        URL="IP_SERVIDOR";
+        time_stamp=$(date +"%Y-%m-%d %H:%M");
+        res=$(curl -sS -I $URL 2>&1 | head -n 1);
+
+        if [[ ! $res =~ HTTP ]]; then
+                notificacao "[$time_stamp] Erro ao tentar acessar o servidor: \n$res"
+        fi
+
+        echo -e "---\n[$time_stamp]:\n$res" >> /var/log/monitoramento.log
+}
+
+
+notificacao(){
+        WEBHOOK_URL="URL"
+        mensagem=$(echo -e $1 | jq -Rs .)
+        conteudo=$(cat << EOF
+{
+        "content": $mensagem
+}
+EOF
+)
+        curl --json "$conteudo" $WEBHOOK_URL
+}
+
+get_response
